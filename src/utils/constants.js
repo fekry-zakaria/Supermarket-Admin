06f5d6c7973ae20e876b2a7f31d02353f33d286d
fekry <fekry.zakaria1461998@gmail.com>
2025-12-31 03:44:04 +0200

    Initial React template

---
+++ src/utils/constants.js
@@ -0,0 +1,25 @@
+export const APP_NAME = 'My App';
+
+export const ROUTES = {
+  HOME: '/',
+  LOGIN: '/login',
+  NOT_FOUND: '*',
+};
+
+export const API_ENDPOINTS = {
+  AUTH: {
+    LOGIN: '/auth/login',
+    LOGOUT: '/auth/logout',
+    REGISTER: '/auth/register',
+  },
+  USER: {
+    PROFILE: '/user/profile',
+    UPDATE: '/user/update',
+  },
+};
+
+export const STORAGE_KEYS = {
+  TOKEN: 'token',
+  USER: 'user',
+};
+
